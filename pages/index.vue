6440f3c86787c43a9f9f9746d16f9020cd3be6fa
tannerbaum <hoisington.tanner@gmail.com>
2023-11-12 23:58:50 -0500

    home page skeleton

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts"></script>
 <template>
-  <Layout> Home </Layout>
+  <Layout>
+    <h1 class="text-4xl">Customer Management Inc</h1>
+    <p class="text-xl">Welcome to the Customer Management Inc</p>
+    <div>Recent Feedback:</div>
+    <div class="flex justify-center gap-12">
+      <div class="bg-slate-400">
+        <h2>View others' feedback</h2>
+        <NuxtLink to="/feedback/view" class="bg-blue-900"> View </NuxtLink>
+      </div>
+      <div class="bg-slate-400">
+        <h2>Submit your own feedback</h2>
+        <NuxtLink to="/feedback/submit" class="bg-blue-900">
+          Submit Feedback
+        </NuxtLink>
+      </div>
+    </div>
+  </Layout>
 </template>
